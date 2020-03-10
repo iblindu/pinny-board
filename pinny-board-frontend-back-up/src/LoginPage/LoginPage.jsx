248f35fc43dafefb2e5c58b0daf18b9e68cc4133
IuliaBlindu <iuliaioana.blindu@gmail.com>
2020-03-11 00:09:10 +0200

    Functioneaza backend-ul dar frontend-ul trebuie refacut desi merge ok

---
+++ pinny-board-frontend-back-up/src/LoginPage/LoginPage.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { userActions } from "../_actions";
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // reset login status
+    this.props.logout();
+
+    this.state = {
+      username: "",
+      password: "",
+      submitted: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ submitted: true });
+    const { username, password } = this.state;
+    if (username && password) {
+      this.props.login(username, password);
+    }
+  }
+
+  render() {
+    const titleDivStyle = {
+      backgroundColor: "white",
+      height: "50vh",
+      width: "100vw",
+      lineHeight: "50vh",
+      paddingTop: "20vh"
+    };
+
+    const titleStyle = {
+      color: "green",
+      fontSize: "10vw",
+      textAlign: "center"
+    };
+
+    const formDivStyle = {
+      height: "40%",
+      width: "50%",
+      margin: "auto"
+    };
+
+    const { loggingIn } = this.props;
+    const { username, password, submitted } = this.state;
+    return (
+      <div
+        style={{
+          height: "100%",
+          width: "100%"
+        }}
+      >
+        <div className="container" style={titleDivStyle}>
+          <h1 style={titleStyle}>microsera</h1>
+        </div>
+
+        <div className="container" style={formDivStyle}>
+          <div className="col-md-auto">
+            <form name="form" onSubmit={this.handleSubmit}>
+              <div
+                className={
+                  "form-group" + (submitted && !username ? " has-error" : "")
+                }
+              >
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  placeholder="username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+                {submitted && !username && (
+                  <div className="help-block">Username is required</div>
+                )}
+              </div>
+              <div
+                className={
+                  "form-group" + (submitted && !password ? " has-error" : "")
+                }
+              >
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                {submitted && !password && (
+                  <div className="help-block">Password is required</div>
+                )}
+              </div>
+              <div className="form-group">
+                <button className="btn btn-outline-primary">Login</button>
+                {loggingIn && (
+                  <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                )}
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapState(state) {
+  const { loggingIn } = state.authentication;
+  return { loggingIn };
+}
+
+const actionCreators = {
+  login: userActions.login,
+  logout: userActions.logout
+};
+
+const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
+export { connectedLoginPage as LoginPage };
---
+++ pinny-board-frontend-back-up/src/_components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Navbar extends Component {
+  render() {
+    return <nav className="navbar navbar-dark bg-dark navbar-expand-lg"></nav>;
+  }
+}
+
+export default Navbar;
